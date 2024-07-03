3a841a49332ba23ef645c065f7a3e753dfdd104b
Steve Nguyen <sonntuet1997@gmail.com>
2024-07-03 10:10:17 +0000

    Allow partial message encryption

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,36 @@ use std::io;
 use tfhe::boolean::prelude::*;
 use padding::pad_sha256_input;
 use sha256::{sha256_fhe, bools_to_hex};
+use crate::padding::{compute_padding, string_as_bool_vec};
 
 fn main() {
     // INTRODUCE INPUT FROM STDIN
 
-    let mut input = String::new();
-    println!("Write input to hash:");
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-
-    input = input.trim_end_matches('\n').to_string();
-
-    println!("You entered: \"{}\"", input);
-
+    let i = "12";
+    let i2 = "34";
+    let length_i2 = 16;
     // CLIENT PADS DATA AND ENCRYPTS IT
 
     let (ck, sk) = gen_keys();
 
-    let padded_input = pad_sha256_input(&input);
-    let encrypted_input = encrypt_bools(&padded_input, &ck);
 
+    let i_bool = string_as_bool_vec(i);
+    let i2_bool = string_as_bool_vec(i2);
+
+    let mut encrypted_i1 = encrypt_bools(&i_bool, &ck);
+    let encrypted_i2 = encrypt_bools(&i2_bool, &ck);
+
+    let padded_path = compute_padding(i_bool.len(), length_i2);
+    eprintln!("padded_path.len() = {:#?}", padded_path.len());
+    eprintln!("padded_path = {:?}", padded_path);
+    let encrypted_padded_path = encrypt_bools(&padded_path, &ck);
     // SERVER COMPUTES OVER THE ENCRYPTED PADDED DATA
 
+    encrypted_i1.extend(encrypted_i2.iter().cloned());
+    encrypted_i1.extend(encrypted_padded_path.iter().cloned());
+    eprintln!("encrypted_i1.len() = {:#?}", encrypted_i1.len());
     println!("Computing the hash");
-    let encrypted_output = sha256_fhe(encrypted_input, &sk);
+    let encrypted_output = sha256_fhe(encrypted_i1, &sk);
 
     // CLIENT DECRYPTS THE OUTPUT
 
--- src/padding.rs
+++ src/padding.rs
@@ -6,24 +6,33 @@
 // If input starts with "0x" and following characters are valid hexadecimal values, it's interpreted
 // as hex, otherwise input is interpreted as text
 pub fn pad_sha256_input(input: &str) -> Vec<bool> {
-    let bytes = if input.starts_with("0x") && is_valid_hex(&input[2..]) {
-        let no_prefix = &input[2..];
-        let hex_input = if no_prefix.len() % 2 == 0 { // hex value can be converted to bytes
-            no_prefix.to_string()
-        } else {
-            format!("0{}", no_prefix) // pad hex value to ensure a correct conversion to bytes
-        };
-        hex_input
-            .as_bytes()
-            .chunks(2)
-            .map(|chunk| u8::from_str_radix(std::str::from_utf8(chunk).unwrap(), 16).unwrap())
-            .collect::<Vec<u8>>()
-    } else {
-        input.as_bytes().to_vec()
-    };
-
-    pad_sha256_data(&bytes)
+    let input_bytes = input.as_bytes().to_vec();
+    pad_sha256_data(&input_bytes)
 }
+
+pub fn compute_padding(length_i1: usize, length_i2: usize) -> Vec<bool> {
+    // Append a single '1' bit
+    let mut bits = vec![true];
+
+    // Calculate the number of padding zeros required
+    let padding_zeros = (512 - ((length_i1 + length_i2 + 1 + 64) % 512)) % 512;
+    eprintln!("padding_zeros = {:#?}", padding_zeros);
+    bits.extend(std::iter::repeat(false).take(padding_zeros));
+
+    // Append a 64-bit big-endian representation of the original message length
+    let data_len_bits = (length_i1 as u64) + (length_i2 as u64);
+    eprintln!("data_len_bits = {:#?}", data_len_bits);
+    bits.extend((0..64).rev().map(|i| (data_len_bits >> i) & 1 == 1));
+
+    bits
+}
+
+
+pub fn string_as_bool_vec(input: &str) -> Vec<bool> {
+    let input_bytes = input.as_bytes().to_vec();
+    input_bytes.iter().flat_map(|byte| (0..8).rev().map(move |i| (byte >> i) & 1 == 1)).collect::<Vec<_>>()
+}
+
 
 fn is_valid_hex(hex: &str) -> bool {
     hex.chars().all(|c| c.is_digit(16))
@@ -41,6 +50,7 @@ fn pad_sha256_data(data: &[u8]) -> Vec<bool> {
 
     // Append a 64-bit big-endian representation of the original message length
     let data_len_bits = (data.len() as u64) * 8;
+    eprintln!("data_len_bits = {:#?}", data_len_bits);
     bits.extend((0..64).rev().map(|i| (data_len_bits >> i) & 1 == 1));
 
     bits
@@ -53,7 +63,6 @@ mod tests {
 
     #[test]
     fn test_pad_sha256_input() {
-
         let input = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq";
         let expected_output = "6162636462636465636465666465666765666768666768696768696a68696a6\
         b696a6b6c6a6b6c6d6b6c6d6e6c6d6e6f6d6e6f706e6f70718000000000000000000000000000000000000000000\
